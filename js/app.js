ead715d07d291f76f622c5c6b5f72cb4bd08cc8c
Nakorn Sinpadung <nakorndev@gmail.com>
2021-06-05 13:31:08 +0700

    Add sample

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// / = root
+// ./ = current
+// ../ = parent
+
+import { plus, minus, name, age } from './math.js'
+
+console.log(plus(4, 2))
+console.log(minus(4, 2))
+console.log(name)
+console.log(age)
---
+++ js/math.js
@@ -0,0 +1,21 @@
+// export default class MyCustomMath {
+//     static plus(a, b) {
+//         return a + b
+//     }
+//     static minus(a, b) {
+//         return a - b
+//     }
+// }
+
+export function plus(a, b) {
+    return a + b
+}
+
+export function minus(a, b) {
+    return a - b
+}
+
+export const name = 'John Doe'
+export const age = 18
+
+export default 'Hello'
